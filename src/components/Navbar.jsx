d8539d5fb33b9bee092f328149b439a8ccbfc731
shaheersystems <shaheer.imran1429@gmail.com>
2022-06-07 23:35:42 +0500

    Navabr added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Navbar(props) {
+    return <div className="nav-wrap">
+        <div className="nav">
+            <div className="logo">
+                <h2>Process.</h2>
+            </div>
+            <div className="btns">
+                <button>Portfolio</button>
+            </div>
+        </div>
+    </div >
+}
+export default Navbar;
